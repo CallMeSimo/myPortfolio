672c4d2fee348262e86445099cd56cccf5afe1e9
CallMeSimo <elhaddaouiwassim@gmail.com>
2024-03-22 01:47:29 +0100

    forgot the email :/

--- src/app/Components/EmailSection.jsx
+++ src/app/Components/EmailSection.jsx
@@ -84,7 +84,7 @@ const EmailSection = () => {
                 id="email"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="jacob@google.com"
+                placeholder="your@mail.com"
               />
             </div>
             <div className="mb-6">
@@ -100,13 +100,13 @@ const EmailSection = () => {
                 id="subject"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Just saying hi"
               />
             </div>
             <div className="mb-6">
               <label
                 htmlFor="message"
                 className="text-white block text-sm mb-2 font-medium"
+                placeholder="Just say hello!"
               >
                 Message
               </label>
--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -5,16 +5,17 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
+  console.log("test");
   const { email, subject, message } = await req.json();
   try {
     const data = await resend.emails.send({
-      from: "simoportfolio@gmail.dev",
+      from: "Wassim@simoportfolio.com",
       to: [fromEmail, email],
       subject: subject,
       react: (
         <>
           <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
+          <p>Thank you for contacting me!</p>
           <p>New message submitted:</p>
           <p>{message}</p>
         </>
